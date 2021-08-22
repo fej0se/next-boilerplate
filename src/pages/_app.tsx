9c50603aa5f525b71623cec1c5ea5772ea57ebe2
Felipe José <felipej0se@icloud.com>
2021-08-22 12:19:23 -0300

    feat: pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Next.js boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/images/512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
+        <meta name="theme-color" content="#fefefe" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
